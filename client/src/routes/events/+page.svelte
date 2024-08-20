0b4b12b39c75c210acd77e2ec1878bdc199ca821
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-08-20 16:20:13 +0300

    tailwind responsive class fix

--- client/src/routes/events/+page.svelte
+++ client/src/routes/events/+page.svelte
@@ -12,19 +12,23 @@
 	<p>No events found</p>
 {:else}
 	<Wrapper>
-		<div class="flex-lg-row flex flex-col">
-			<div class="w--lg-1/5 w-full px-3 mb-3">
+		<div class="lg:flex-row flex flex-col">
+			<div class="mb-3 w-full px-3 lg:w-1/5">
 				{formattedDate}
 			</div>
-			<div class="w-lg-4/5 w-full px-3">
+			<div class="w-full px-3 lg:w-4/5">
 				{#each data.events as event}
-					<div class="flex-lg-row mb-16 flex flex-col gap-6">
-						<img src={event.image_url} alt={event.title} class="w-full w-lg-1/3 rounded-sm object-cover" />
+					<div class="mb-16 flex flex-col gap-6 lg:flex-row">
+						<img
+							src={event.image_url}
+							alt={event.title}
+							class="lg:w-1/3 w-full rounded-sm object-cover"
+						/>
 						<div class="flex flex-col gap-4">
-							<h2 class="text-foreground text-lg-xl text-lg font-medium">
+							<h2 class="text-foreground text-lg font-medium lg:text-xl">
 								{event.title}
 							</h2>
-							<div class="text-muted-foreground flex flex-col gap-2 text-lg-lg text-base">
+							<div class="text-muted-foreground flex flex-col gap-2 text-base lg:text-lg">
 								{@html event.description}
 							</div>
 						</div>
