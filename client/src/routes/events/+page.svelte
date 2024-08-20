77282c0aa34a68c88a86bab9dcd58e2b60f3156e
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-08-20 16:17:44 +0300

    Events page responsive fix

--- client/src/routes/events/+page.svelte
+++ client/src/routes/events/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Wrapper from '$lib/components/ui/Wrapper.svelte';
-
+	const formattedDate = new Intl.DateTimeFormat('tr-TR', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric'
+	}).format(new Date());
 	export let data;
 </script>
 
@@ -8,20 +12,19 @@
 	<p>No events found</p>
 {:else}
 	<Wrapper>
-		<div class="flex flex-row">
-			<div class="w-1/5 px-3">
-				{new Date().getDay()}
-				{new Date().getMonth()}
+		<div class="flex-lg-row flex flex-col">
+			<div class="w--lg-1/5 w-full px-3 mb-3">
+				{formattedDate}
 			</div>
-			<div class="w-4/5 px-3">
+			<div class="w-lg-4/5 w-full px-3">
 				{#each data.events as event}
-					<div class="mb-16 flex gap-6">
-						<img src={event.image_url} alt={event.title} class="w-1/3 rounded-sm object-cover" />
+					<div class="flex-lg-row mb-16 flex flex-col gap-6">
+						<img src={event.image_url} alt={event.title} class="w-full w-lg-1/3 rounded-sm object-cover" />
 						<div class="flex flex-col gap-4">
-							<h2 class="text-foreground text-xl font-medium">
+							<h2 class="text-foreground text-lg-xl text-lg font-medium">
 								{event.title}
 							</h2>
-							<div class="text-muted-foreground flex flex-col gap-2 text-lg">
+							<div class="text-muted-foreground flex flex-col gap-2 text-lg-lg text-base">
 								{@html event.description}
 							</div>
 						</div>
